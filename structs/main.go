4137a5bdf260df5a155c234f84bbf4fb76c32e40
Ayush Dubey <ayushdubey70@gmail.com>
2020-08-10 21:49:00 +0530

    structs: declaring embeded structs [1/2]
    *the first type of declaring a embedded struct
    
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- structs/main.go
+++ structs/main.go
@@ -2,12 +2,26 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
-	ayush := person{firstName: "Ayush", lastName: "Dubey"}
-	fmt.Println(ayush)
+	ayush := person{
+		firstName: "Ayush",
+		lastName:  "Dubey",
+		contact: contactInfo{
+			email:   "ayushdubey70@gmail.com",
+			zipCode: 700002,
+		},
+	}
+
+	fmt.Printf("%+v", ayush)
 }
